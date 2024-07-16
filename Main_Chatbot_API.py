52a01cd10d3b86a3fa67d6e4109c1b2171dbb369
Abd ElRahman Menisy <90526615+Abdelrahman-Menisy@users.noreply.github.com>
2024-07-16 13:42:48 +0300

    Update Main_Chatbot_API.py

--- Main_Chatbot_API.py
+++ Main_Chatbot_API.py
@@ -13,9 +13,9 @@ from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
 import base64
 import io
+
 nltk.download('wordnet')
 nltk.download('punkt')
-
 
 model = tf.keras.models.load_model(
     r"chatbot_model_v5.h5", custom_objects=None, compile=True, safe_mode=True
@@ -34,217 +34,125 @@ app.add_middleware(
 )
 
 class model_input(BaseModel):
-    
     msg: str
-    
+
 lemmatizer = WordNetLemmatizer()
 
-
 intents = json.loads(open(r"intents.json").read())
- 
+
 words = pickle.load(open(r"words_v5.pkl", 'rb'))
 classes = pickle.load(open(r"classes_v5.pkl", 'rb'))
 
-
+# Error messages dictionary
+error_messages = {
+    "no_message": "No message provided. Please enter a message.",
+    "speech_recognition_error": "Unable to recognize speech. Please try again.",
+    "speech_recognition_service_error": "Speech recognition service error. Please check your internet connection.",
+    "general_error": "An error occurred. Please try again later.",
+    "file_format_error": "File must be in WAV format."
+}
 
 def clean_up_sentence(sentence):
-	"""
-	Takes a sentence as input, tokenizes the sentence into words, lemmatizes each word, and returns a list of lemmatized words.
-	"""
-	sentence_words = nltk.word_tokenize(sentence)
-	sentence_words = [lemmatizer.lemmatize(word)
-					for word in sentence_words]
-
-	return sentence_words
-
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
 
 def bag_of_words(sentence):
-	"""
-	Generate a bag of words representation for the given sentence.
-
-	Parameters:
-	- sentence: a string representing the input sentence
-
-	Return:
-	- np.array: an array representing the bag of words for the given sentence
-	"""
-	sentence_words = clean_up_sentence(sentence)
-	bag = [0] * len(words)
-
-	for w in sentence_words:
-		for i, word in enumerate(words):
-			if word == w:
-				bag[i] = 1
-	return np.array(bag)
-
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
 
 def predict_class(sentence):
-    """
-    Generates predictions for the class of a given sentence using a bag of words approach.
-    """
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
     results.sort(key=lambda x: x[1], reverse=True)
-
     return_list = []
-
     if results:
         for r in results:
             return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
-
-
 def get_response(intents_list, intents_json):
-    """
-    Get response function: 
-    - Parameters: intents_list (list), intents_json (json)
-    - Return type: string
-    """
     if not intents_list:
-        return {"text_response": "Sorry, I didn't understand that."}
-
+        return {"user_message": "Sorry, I didn't understand that."}
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
-
     result = ''
-
     for i in list_of_intents:
         if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
     return result
 
-
-
 def process_text_message(txt):
-    """
-    Processes a text message by predicting the class of the message, getting a response, and returning the result.
-    """
     try:
         predict = predict_class(txt)
         if not predict:
-            return {"text_response": "Sorry, I didn't understand that."}
+            return {"user_message": "Sorry, I didn't understand that."}
         res = get_response(predict, intents)
         return res
     except Exception as e:
-        return {"text_response": f"Error processing text message: {str(e)}"}
-
-
+        return {"user_message": error_messages["general_error"]}
 
 def process_voice_to_text_message(audio_data):
-    """
-    A function that processes audio data to convert it into text using Google's Speech Recognition API.
-
-    Parameters:
-    - audio_data: The audio data to be converted to text.
-
-    Returns:
-    - The recognized text from the audio data.
-    - Raises HTTPException with status code 400 if speech recognition fails to recognize the speech.
-    - Raises HTTPException with status code 500 if there's an error with the speech recognition service.
-    """
     recognizer = sr.Recognizer()
     try:
-        # Convert binary audio data to AudioData instance
         with io.BytesIO(audio_data) as f:
             audio = sr.AudioFile(f)
             with audio as source:
                 audio_data = recognizer.record(source)
-        # Recognize speech using Google's Speech Recognition API
         text = recognizer.recognize_google(audio_data, language='en')
         return text
     except sr.UnknownValueError:
-        return {"text_response": "Unable to recognize speech"}
-    except sr.RequestError as e:
-        return {"text_response": "Speech recognition service error, check your internet connection"}
+        return {"user_message": error_messages["speech_recognition_error"]}
+    except sr.RequestError:
+        return {"user_message": error_messages["speech_recognition_service_error"]}
     except Exception as e:
-        return {"text_response": f"Error processing voice message: {str(e)}"}
-
-
+        return {"user_message": error_messages["general_error"]}
 
 def text_to_speech(text):
-	# sourcery skip: inline-immediately-returned-variable
-  """
-  Generates a text-to-speech audio file from the input text in the specified language.
-
-  Parameters:
-    text (str): The text to be converted to speech.
-    language (str): The language in which the text should be spoken.
-
-  Returns:
-    str: The filename of the saved audio file.
-  """
-  file_name = f"output_{'en'}.mp3"
-  output = gTTS(text, lang='en', slow=False)
-  output.save(file_name)
-  return file_name
-
-
-
+    file_name = f"output_{'en'}.mp3"
+    output = gTTS(text, lang='en', slow=False)
+    output.save(file_name)
+    return file_name
 
 @app.post("/medi_text")
 def process_medi_message(user_message: model_input):
-
     try:
         text_message = user_message.msg.lower()
         if not text_message:
-            return {"text_response": "No message provided"}
-        
+            return {"user_message": error_messages["no_message"]}
         text_response = process_text_message(text_message)
         voice_response = text_to_speech(text_response)
-        
     except Exception as e:
-        return {"text_response": str(e)}
-
-    # Combine text and audio into a single JSON response
-    response_data = {"user_message": text_message, "text_response": text_response, "voice_response": voice_response}
-
-    # Encode the audio data
+        return {"user_message": error_messages["general_error"]}
     with open(voice_response, "rb") as f:
         encoded_content = base64.b64encode(f.read()).decode("utf-8")
-
-    # Combine text and audio into a single JSON response
     response_data = {"user_message": text_message, "text_response": text_response, "voice_response": encoded_content}
-
     return response_data
-
-
 
 @app.post("/medi_voice")
 async def process_medi_message(file: UploadFile = File(...)):
     try:
-        # Check WAV format
         if file.filename.endswith('.wav'):
             audio_data = await file.read()
             text_message = process_voice_to_text_message(audio_data)
-            
             if isinstance(text_message, dict):
-                # Handle the case where speech recognition failed
                 return JSONResponse(content=text_message, status_code=400)
-            
-            text_message = text_message.lower()  # Ensure text_message is lowercase
+            text_message = text_message.lower()
             text_response = process_text_message(text_message)
             voice_response = text_to_speech(text_response)
-            
         else:
-            return {"text_response": "File must be in WAV format"}
-
+            return {"user_message": error_messages["file_format_error"]}
     except Exception as e:
-        return {"text_response": str(e)}
-    
-    # Encode the audio data
+        return {"user_message": error_messages["general_error"]}
     with open(voice_response, "rb") as f:
         encoded_content = base64.b64encode(f.read()).decode("utf-8")
-    
-    # Combine text and audio into a single JSON response
     response_data = {"user_message": text_message, "text_response": text_response, "voice_response": encoded_content}
-    
     return JSONResponse(content=response_data)
-
-
-
